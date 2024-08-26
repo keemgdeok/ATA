91e8c72c735775883e0e1f7e3f53b2292f46b94e
keemgdeok <keemgdeok@gmail.com>
2024-08-26 19:14:14 +0900

    chore:

--- ATA/API.sh
+++ ATA/API.sh
@@ -4,6 +4,5 @@
 venv_activate=$(wslpath -w "/mnt/c/Users/api/apivenv/Scripts/activate.bat")
 script_path=$(wslpath -w "/mnt/c/Users/api/main.py")
 
-# # Activate the virual enviroment and run the script
-# #cmd.exe /c "$venv_activate && python $script_path"
+# Activate the virual enviroment and run the script
 /mnt/c/Windows/System32/cmd.exe /c "$venv_activate && python $script_path"
