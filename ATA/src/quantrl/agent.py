742f801f1f3d8065f18ba693f7a14318505ba909
keemgdeok <keemgdeok@gmail.com>
2024-08-26 17:51:36 +0900

    chore: change folder name

---
+++ ATA/src/quantrl/agent.py
@@ -0,0 +1,206 @@
+import os
+import sys
+import numpy as np
+
+from quantrl import utils
+
+class Agent:
+    # 에이전트 상태가 구성하는 값 개수
+    STATE_DIM = 3
+
+    # 매매 수수료 및 세금
+    TRADING_CHARGE = 0.00015  # 거래 수수료 0.015%
+    TRADING_TAX = 0.002  # 거래세 0.2%
+
+    # 행동
+    ACTION_BUY = 0  # 매수
+    ACTION_SELL = 1  # 매도
+    ACTION_HOLD = 2  # 관망
+    ACTIONS = [ACTION_BUY, ACTION_SELL, ACTION_HOLD]
+    NUM_ACTIONS = len(ACTIONS)  # 인공 신경망에서 고려할 출력값의 개수
+
+    def __init__(self, environment, initial_balance, min_trading_price, max_trading_price):
+        self.environment = environment
+        self.initial_balance = initial_balance  # 초기 자본금
+
+        self.min_trading_price = min_trading_price
+        self.max_trading_price = max_trading_price
+
+        # Agent 클래스의 속성
+        self.balance = initial_balance  # 현재 현금 잔고
+        self.num_stocks = 0  # 보유 주식 수
+        self.portfolio_value = initial_balance
+        self.num_buy = 0  # 매수 횟수
+        self.num_sell = 0  # 매도 횟수
+        self.num_hold = 0  # 관망 횟수
+
+        # Agent 클래스의 상태
+        self.ratio_hold = 0  # 주식 보유 비율
+        self.profitloss = 0  # 손익률
+        self.avg_buy_price = 0  # 주당 매수 단가
+        self.past_portfolio_values = []
+
+    def reset(self):
+        self.balance = self.initial_balance
+        self.num_stocks = 0
+        self.portfolio_value = self.initial_balance
+        self.num_buy = 0
+        self.num_sell = 0
+        self.num_hold = 0
+        self.ratio_hold = 0
+        self.profitloss = 0
+        self.avg_buy_price = 0
+        self.past_portfolio_values = []
+
+    def set_balance(self, balance):
+        self.initial_balance = balance
+
+    def get_states(self):
+        self.ratio_hold = self.num_stocks * self.environment.get_price() / self.portfolio_value
+        return (
+            self.ratio_hold,
+            self.profitloss,
+            (self.environment.get_price() / self.avg_buy_price) - 1 if self.avg_buy_price > 0 else 0
+        )
+
+    def decide_action(self, pred_value, pred_policy, epsilon):
+        confidence = 0.
+
+        pred = pred_policy
+        if pred is None:
+            pred = pred_value
+
+        if pred is None:
+            # 예측 값이 없을 경우 탐험
+            epsilon = 1
+        else:
+            # 값이 모두 같은 경우 탐험
+            maxpred = np.max(pred)
+            if (pred == maxpred).all():
+                epsilon = 1
+
+            # if pred_policy is not None:
+            #     if np.max(pred_policy) - np.min(pred_policy) < 0.05:
+            #         epsilon = 1
+
+        # 탐험 결정
+        if np.random.rand() < epsilon:
+            exploration = True
+            action = np.random.randint(self.NUM_ACTIONS)
+        else:
+            exploration = False
+            action = np.argmax(pred)
+
+        confidence = .5
+        if pred_policy is not None:
+            confidence = pred[action]
+        elif pred_value is not None:
+            confidence = utils.sigmoid(pred[action])
+
+        return action, confidence, exploration
+
+    def validate_action(self, action):
+        if action == Agent.ACTION_BUY:
+            if self.balance < self.environment.get_price() * (1 + self.TRADING_CHARGE):
+                return False
+        elif action == Agent.ACTION_SELL:
+            if self.num_stocks <= 0:
+                return False
+        return True
+
+    def decide_trading_unit(self, confidence):
+        if confidence is None:
+            return self.min_trading_price
+        added_trading_price = max(min(
+            int(confidence * (self.max_trading_price - self.min_trading_price)),
+            self.max_trading_price - self.min_trading_price), 0)
+        trading_price = self.min_trading_price + added_trading_price
+        return max(int(trading_price / self.environment.get_price()), 1)
+
+    def calculate_reward(self):
+        # Calculate the reward based on the change in portfolio value
+        current_portfolio_value = self.portfolio_value
+        previous_portfolio_value = self.past_portfolio_values[-1] if self.past_portfolio_values else self.initial_balance
+
+        # Reward is the change in portfolio value
+        reward = current_portfolio_value - previous_portfolio_value
+
+        # Update the past portfolio values list
+        self.past_portfolio_values.append(current_portfolio_value)
+
+        reward *= 10
+        #reward /= self.initial_balance
+        # Normalize the reward (optional)
+        reward = np.clip(reward / self.initial_balance, -1, 1)
+        
+        return reward
+
+    def act(self, action, confidence):
+        if not self.validate_action(action):
+            action = Agent.ACTION_HOLD
+
+        # 환경에서 현재 가격 얻기
+        curr_price = self.environment.get_price()
+
+        # 매수
+        if action == Agent.ACTION_BUY:
+            trading_unit = self.decide_trading_unit(confidence)
+            balance = (
+                self.balance - curr_price *
+                (1 + self.TRADING_CHARGE) * trading_unit
+            )
+            if balance < 0:
+                trading_unit = min(
+                    int(self.balance / (curr_price * (1 + self.TRADING_CHARGE))),
+                    int(self.max_trading_price / curr_price)
+                )
+            invest_amount = curr_price * (1 + self.TRADING_CHARGE) * trading_unit
+            if invest_amount > 0:
+                self.avg_buy_price = \
+                    (self.avg_buy_price * self.num_stocks + curr_price * trading_unit) \
+                    / (self.num_stocks + trading_unit)
+                self.balance -= invest_amount
+                self.num_stocks += trading_unit
+                self.num_buy += 1
+
+        # 매도
+        # elif action == Agent.ACTION_SELL:
+        #     trading_unit = self.decide_trading_unit(confidence)
+        #     trading_unit = min(trading_unit, self.num_stocks)
+        #     invest_amount = curr_price * (
+        #         1 - (self.TRADING_TAX + self.TRADING_CHARGE)) * trading_unit
+        #     if invest_amount > 0:
+        #         self.avg_buy_price = \
+        #             (self.avg_buy_price * self.num_stocks - curr_price * trading_unit) \
+        #             / (self.num_stocks - trading_unit) \
+        #             if self.num_stocks > trading_unit else 0
+        #         self.num_stocks -= trading_unit
+        #         self.balance += invest_amount
+        #         self.num_sell += 1
+        
+        elif action == Agent.ACTION_SELL:
+            trading_unit = self.decide_trading_unit(confidence)
+            trading_unit = min(trading_unit, self.num_stocks)
+            invest_amount = curr_price * (
+                1 - (self.TRADING_TAX + self.TRADING_CHARGE)) * trading_unit
+
+            if invest_amount > 0:
+                self.num_stocks -= trading_unit
+                self.balance += invest_amount
+                self.num_sell += 1
+                
+                if self.num_stocks == 0:
+                    self.avg_buy_price = 0  # Reset avg_buy_price when all stocks are sold
+
+        # 관망
+        elif action == Agent.ACTION_HOLD:
+            self.num_hold += 1
+
+        # 포트폴리오 가치 갱신
+        self.portfolio_value = self.balance + curr_price * self.num_stocks
+        self.profitloss = self.portfolio_value / self.initial_balance - 1
+
+        # Calculate the reward based on FinRL structure
+        reward = self.calculate_reward()
+        
+        return reward
---
+++ ATA/src/quantrl/sacd/memory/base.py
@@ -0,0 +1,175 @@
+from collections import deque
+import numpy as np
+import torch
+import threading
+
+class MultiStepBuff:
+
+    def __init__(self, maxlen=3):
+        super(MultiStepBuff, self).__init__()
+        self.maxlen = int(maxlen)
+        self.lock = threading.Lock()
+        self.reset()
+
+    def append(self, state, action, reward):
+        self.states.append(state)
+        self.actions.append(action)
+        self.rewards.append(reward)
+
+    def get(self, gamma=0.99):
+        assert len(self.rewards) > 0
+        state = self.states.popleft()
+        action = self.actions.popleft()
+        reward = self._nstep_return(gamma)
+        return state, action, reward
+
+    def _nstep_return(self, gamma):
+        r = np.sum([r * (gamma ** i) for i, r in enumerate(self.rewards)])
+        self.rewards.popleft()
+        return r
+
+    def reset(self):
+        # Buffer to store n-step transitions.
+        self.states = deque(maxlen=self.maxlen)
+        self.actions = deque(maxlen=self.maxlen)
+        self.rewards = deque(maxlen=self.maxlen)
+
+    def is_empty(self):
+        return len(self.rewards) == 0
+
+    def is_full(self):
+        return len(self.rewards) == self.maxlen
+
+    def __len__(self):
+        return len(self.rewards)
+
+
+class LazyMemory(dict):
+
+    def __init__(self, capacity, state_shape, device):
+        super(LazyMemory, self).__init__()
+        self.capacity = int(capacity)
+        self.state_shape = state_shape
+        self.device = device
+        self.lock = threading.Lock()
+        self._n = 0
+        self._p = 0
+
+    def reset(self):
+        with self.lock:  # Ensure thread-safe access
+            print("Resetting memory")
+            self['state'] = []
+            self['next_state'] = []
+            self['action'] = np.empty((self.capacity, 1), dtype=np.int64)
+            self['reward'] = np.empty((self.capacity, 1), dtype=np.float32)
+            self['done'] = np.empty((self.capacity, 1), dtype=np.float32)
+
+            self._n = 0
+            self._p = 0
+
+    def append(self, state, action, reward, next_state, done,
+               episode_done=None):
+        self._append(state, action, reward, next_state, done)
+
+    def _append(self, state, action, reward, next_state, done):
+        with self.lock:  # Ensure thread-safe access
+        
+            if 'state' not in self:
+                self['state'] = []
+            if 'next_state' not in self:
+                self['next_state'] = []
+            if 'action' not in self:
+                self['action'] = np.empty((self.capacity, 1), dtype=np.int64)
+            if 'reward' not in self:
+                self['reward'] = np.empty((self.capacity, 1), dtype=np.float32)
+            if 'done' not in self:
+                self['done'] = np.empty((self.capacity, 1), dtype=np.float32)
+            
+            
+            self['state'].append(state)
+            self['next_state'].append(next_state)
+            self['action'][self._p] = action
+            self['reward'][self._p] = reward
+            self['done'][self._p] = done
+            
+            #print(f"Full actions buffer up to index {self._p}: {self['action'][1]}")
+
+            self._n = min(self._n + 1, self.capacity)
+            self._p = (self._p + 1) % self.capacity
+
+            self.truncate()
+
+    def truncate(self):
+        while len(self['state']) > self.capacity:
+            del self['state'][0]
+            del self['next_state'][0]
+    
+    def __len__(self):
+        return self._n
+    
+    def sample(self, batch_size):
+        
+        if len(self) < batch_size:
+            print("Not enough data to sample a full batch")
+            return None
+
+        # Ensure batch_size number of contiguous samples
+        start_indices = np.arange(0, len(self) - batch_size + 1)
+        selected_start = np.random.choice(start_indices, 1)[0]
+        indices = np.arange(selected_start, selected_start + batch_size)
+        return self._sample(indices, batch_size)
+
+    def _sample(self, indices, batch_size):
+        with self.lock:  # Ensure thread-safe access
+            bias = -self._p if self._n == self.capacity else 0
+
+            states = np.empty((batch_size, *self.state_shape), dtype=np.float32)
+            next_states = np.empty((batch_size, *self.state_shape), dtype=np.float32)
+
+            for i, index in enumerate(indices):
+                _index = np.mod(index + bias, self.capacity)
+                states[i, ...] = self['state'][_index]
+                next_states[i, ...] = self['next_state'][_index]
+
+            states = torch.tensor(states, dtype=torch.float32).to(self.device)
+            next_states = torch.tensor(next_states, dtype=torch.float32).to(self.device)
+            actions = torch.tensor(self['action'][indices], dtype=torch.int64).to(self.device)
+            rewards = torch.tensor(self['reward'][indices], dtype=torch.float32).to(self.device)
+            dones = torch.tensor(self['done'][indices], dtype=torch.float32).to(self.device)
+
+            
+            return {
+                'states': states,
+                'actions': actions,
+                'rewards': rewards,
+                'next_states': next_states,
+                'dones': dones
+            }
+            
+
+class LazyMultiStepMemory(LazyMemory):
+
+    def __init__(self, capacity, state_shape, device, gamma=0.99,
+                 multi_step=3):
+        super(LazyMultiStepMemory, self).__init__(
+            capacity, state_shape, device)
+
+        self.gamma = gamma
+        self.multi_step = int(multi_step)
+        if self.multi_step != 1:
+            self.buff = MultiStepBuff(maxlen=self.multi_step)
+
+    def append(self, state, action, reward, next_state, done):
+        if self.multi_step != 1:
+            self.buff.append(state, action, reward)
+
+            if self.buff.is_full():
+                state, action, reward = self.buff.get(self.gamma)
+                self._append(state, action, reward, next_state, done)
+
+            if done:
+                while not self.buff.is_empty():
+                    state, action, reward = self.buff.get(self.gamma)
+                    self._append(state, action, reward, next_state, done)
+        else:
+            self._append(state, action, reward, next_state, done)
